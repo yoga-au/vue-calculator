874279e16f1d008c20e4be521bb747e867fc2c77
yoga-au <yogaagung.utama@gmail.com>
2021-01-15 17:51:08 +0700

    Change screen behaviour to look like today calculator

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,15 +1,15 @@
 <template>
-  <input type="text" v-model="calculatorOutput">
-  <div>Result {{ result }}</div>
-  <div class="numpad-container">
-	<div class="numpad" @click="clear">AC</div>
-	<div class="numpad" @click="numOne">1</div>
-	<div class="numpad" @click="numTwo">2</div>
-	<div class="numpad" @click="numThree">3</div>
-	<div class="numpad" @click="plus">+</div>
-	<div class="numpad" @click="minus">-</div>
-	<div class="numpad" @click="evaluate">=</div>
-  </div>
+	<div>{{ output }}</div>
+	<input type="text" v-model="calculatorScreen">
+	<div class="numpad-container">
+		<div class="numpad" @click="clear">AC</div>
+		<div class="numpad" @click="numOne">1</div>
+		<div class="numpad" @click="numTwo">2</div>
+		<div class="numpad" @click="numThree">3</div>
+		<div class="numpad" @click="plus">+</div>
+		<div class="numpad" @click="minus">-</div>
+		<div class="numpad" @click="evaluate">=</div>
+	</div>
 </template>
 
 <script>
@@ -18,38 +18,46 @@ import { Parser } from 'expr-eval'
 export default {
 	data() {
 		return {
-			calculatorOutput: '',
+			calculatorScreen: '',
 			result: '',
+			output: ''
 		}
 	},
 
 	updated() {
-		console.log(this.calculatorOutput)
+		console.log(this.output)
 	},
 
 	methods: {
 		clear() {
-			this.calculatorOutput = ''
+			this.calculatorScreen = ''
 			this.result = ''
 		},
 		numOne() {
-			this.calculatorOutput = this.calculatorOutput.concat('1')
+			this.calculatorScreen = this.calculatorScreen.concat('1')
 		},
 		numTwo() {
-			this.calculatorOutput = this.calculatorOutput.concat('2')
+			this.calculatorScreen = this.calculatorScreen.concat('2')
 		},
 		numThree() {
-			this.calculatorOutput = this.calculatorOutput.concat('3')
+			this.calculatorScreen = this.calculatorScreen.concat('3')
 		},
 		plus() {
-			this.calculatorOutput = this.calculatorOutput.concat(' + ')
+			this.calculatorScreen = this.calculatorScreen.concat(' + ')
+			this.output = this.output.concat(this.calculatorScreen)
+			this.calculatorScreen = ''
 		},
 		minus() {
-			this.calculatorOutput = this.calculatorOutput.concat(' - ')
+			this.calculatorScreen = this.calculatorScreen.concat(' - ')
+			this.output = this.output.concat(this.calculatorScreen)
+			this.calculatorScreen = ''
 		},
 		evaluate() {
-			this.result = Parser.evaluate(this.calculatorOutput)
-			console.log(this.result)
+			this.output = this.output.concat(this.calculatorScreen)
+			// this.calculatorScreen = ''
+			this.result = Parser.evaluate(this.output)
+			this.calculatorScreen = this.result
+			// console.log(this.calculatorScreen)
 		}
 	}
 }
