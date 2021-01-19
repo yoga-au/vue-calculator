9f701460e47d8d30004ba59b4de1a1cefa980f4f
yoga-au <yogaagung.utama@gmail.com>
2021-01-19 11:59:25 +0700

    add disable in operator button

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -6,8 +6,8 @@
 		<button class="numpad" @click="numOne" :disabled="disable">1</button>
 		<button class="numpad" @click="numTwo" :disabled="disable">2</button>
 		<button class="numpad" @click="numThree" :disabled="disable">3</button>
-		<button class="numpad" @click="plus" :disabled="disable">+</button>
-		<button class="numpad" @click="minus" :disabled="disable">-</button>
+		<button class="numpad" @click="plus" :disabled="disableOperator">+</button>
+		<button class="numpad" @click="minus" :disabled="disableOperator">-</button>
 		<button class="numpad" @click="evaluate" :disabled="disable">=</button>
 	</div>
 </template>
@@ -22,6 +22,7 @@ export default {
 			result: '',
 			output: '',
 			disable: false,
+			disableOperator: false,
 		}
 	},
 
@@ -38,29 +39,35 @@ export default {
 		},
 		numOne() {
 			this.screen = this.screen.concat('1')
+			this.disableOperator = false
 		},
 		numTwo() {
 			this.screen = this.screen.concat('2')
+			this.disableOperator = false
 		},
 		numThree() {
 			this.screen = this.screen.concat('3')
+			this.disableOperator = false
 		},
 		plus() {
 			this.screen = this.screen.concat(' + ')
 			this.output = this.output.concat(this.screen)
 			this.screen = ''
+			this.disableOperator = true
 		},
 		minus() {
 			this.screen = this.screen.concat(' - ')
 			this.output = this.output.concat(this.screen)
 			this.screen = ''
+			this.disableOperator = true
 		},
 		evaluate() {
 			this.output = this.output.concat(this.screen)
 			// this.screen = ''
-			this.result = Parser.evaluate(this.output).toString()
+			this.result = Parser.evaluate(this.output)
 			this.screen = this.result
 			this.disable = true
+			this.disableOperator = true
 			// console.log(this.screen)
 		}
 	}
