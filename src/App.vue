7675228b5d54bba08925c3de39bc42ef0e208dfe
yoga-au <yogaagung.utama@gmail.com>
2021-01-20 15:00:31 +0700

    add title and styling to app component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
-  <Calculator />
+  <h1 class="title">Vue Calculator</h1>
+  <p class="sub-title">A simple calculator build solely with Vue.js</p>
+  <Calculator style="margin-bottom: 2rem" />
+  <!-- <a href="http://v3.vuejs.org/" target="_blank" rel="noopener noreferrer"><img src="./assets/logo.png" width="50" height="50" alt="Vue logo"></a> -->
 </template>
 
 <script>
@@ -14,12 +17,21 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Rubik', sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: white;
-  /* margin-top: 60px; */
-}
+  #app {
+    font-family: 'Rubik', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: white;
+    margin-top: 60px;
+  }
+
+  .title {
+    margin-bottom: 1rem;
+  }
+
+  .sub-title {
+    margin-bottom: 2rem;
+    opacity: .5;
+  }
 </style>
