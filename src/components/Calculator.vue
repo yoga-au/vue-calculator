16f61961fd6a51c4bb4897198b1952d57feb515e
yoga-au <yogaagung.utama@gmail.com>
2021-01-20 12:03:42 +0700

    add style when disabled and coloring in evaluate button

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -4,27 +4,27 @@
 		<input class="screen" type="text" v-model="screen">
 		<div class="numpad-container">
 			<button class="numpad clear" @click="clear">AC</button>
-			<button class="numpad delete" @click="deleteScreen">
+			<button class="numpad delete" :class="{ disabled: disable }" @click="deleteScreen">
 				<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-backspace" viewBox="0 0 16 16">
 					<path d="M5.83 5.146a.5.5 0 0 0 0 .708L7.975 8l-2.147 2.146a.5.5 0 0 0 .707.708l2.147-2.147 2.146 2.147a.5.5 0 0 0 .707-.708L9.39 8l2.146-2.146a.5.5 0 0 0-.707-.708L8.683 7.293 6.536 5.146a.5.5 0 0 0-.707 0z"/>
 					<path d="M13.683 1a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-7.08a2 2 0 0 1-1.519-.698L.241 8.65a1 1 0 0 1 0-1.302L5.084 1.7A2 2 0 0 1 6.603 1h7.08zm-7.08 1a1 1 0 0 0-.76.35L1 8l4.844 5.65a1 1 0 0 0 .759.35h7.08a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1h-7.08z"/>
 				</svg>
 			</button>
-			<button class="numpad operator" @click="plus" :disabled="disableOperator"> + </button>
-			<button class="numpad number" @click="numOne" :disabled="disable"> 1 </button>
-			<button class="numpad number" @click="numTwo" :disabled="disable"> 2 </button>
-			<button class="numpad number" @click="numThree" :disabled="disable"> 3 </button>
-			<button class="numpad operator" @click="minus" :disabled="disableOperator"> - </button>
-			<button class="numpad number" @click="numFour" :disabled="disable"> 4 </button>
-			<button class="numpad number" @click="numFive" :disabled="disable"> 5 </button>
-			<button class="numpad number" @click="numSix" :disabled="disable"> 6 </button>
-			<button class="numpad operator" @click="multiply" :disabled="disableOperator"> x </button>
-			<button class="numpad number" @click="numSeven" :disabled="disable"> 7 </button>
-			<button class="numpad number" @click="numEight" :disabled="disable"> 8 </button>
-			<button class="numpad number" @click="numNine" :disabled="disable"> 9 </button>
-			<button class="numpad operator" @click="division" :disabled="disableOperator"> / </button>
-			<button class="numpad zero" @click="numZero" :disabled="disable"> 0 </button>
-			<button class="numpad" @click="evaluate" :disabled="disable"> = </button>
+			<button class="numpad operator" :class="{ disabled: disable }" @click="plus" :disabled="disableOperator"> + </button>
+			<button class="numpad number" :class="{ disabled: disable }" @click="numOne" :disabled="disable"> 1 </button>
+			<button class="numpad number" :class="{ disabled: disable }" @click="numTwo" :disabled="disable"> 2 </button>
+			<button class="numpad number" :class="{ disabled: disable }" @click="numThree" :disabled="disable"> 3 </button>
+			<button class="numpad operator" :class="{ disabled: disable }" @click="minus" :disabled="disableOperator"> - </button>
+			<button class="numpad number" :class="{ disabled: disable }" @click="numFour" :disabled="disable"> 4 </button>
+			<button class="numpad number" :class="{ disabled: disable }" @click="numFive" :disabled="disable"> 5 </button>
+			<button class="numpad number" :class="{ disabled: disable }" @click="numSix" :disabled="disable"> 6 </button>
+			<button class="numpad operator" :class="{ disabled: disable }" @click="multiply" :disabled="disableOperator"> x </button>
+			<button class="numpad number" :class="{ disabled: disable }" @click="numSeven" :disabled="disable"> 7 </button>
+			<button class="numpad number" :class="{ disabled: disable }" @click="numEight" :disabled="disable"> 8 </button>
+			<button class="numpad number" :class="{ disabled: disable }" @click="numNine" :disabled="disable"> 9 </button>
+			<button class="numpad operator" :class="{ disabled: disable }" @click="division" :disabled="disableOperator"> / </button>
+			<button class="numpad zero" :class="{ disabled: disable }" @click="numZero" :disabled="disable"> 0 </button>
+			<button class="numpad evaluate" :class="{ disabled: disable }" @click="evaluate" :disabled="disable"> = </button>
 		</div>
 	</div>
 </template>
@@ -187,6 +187,15 @@ export default {
 		background: #353234;
 	}
 
+	.numpad.disabled {
+		opacity: .2;
+		cursor: default;
+	}
+
+	.numpad.disabled:hover {
+		background: none;
+	}
+
 	.numpad.clear {
 		grid-column: 1 / span 2;
 		grid-row: 1 / 2;
@@ -198,5 +207,19 @@ export default {
 
 	.numpad.zero {
 		grid-column: 1 / span 3;
+	}
+
+	.numpad.evaluate {
+		background: #41B883;
+		color: black;
+	}
+
+	.numpad.evaluate:hover {
+		background: #278a5d;
+		color: black;
+	}
+
+	.numpad.evaluate.disabled:hover {
+		background: #41B883;
 	}
 </style>
